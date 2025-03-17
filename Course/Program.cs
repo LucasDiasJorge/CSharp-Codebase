461f20e6042315331fea6789e526c858f185539e
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-03-16 21:57:41 -0300

    Assignment of Microsoft Course

---
+++ Course/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Course;
+
+// https://www.coursera.org/learn/foundations-of-coding-back-end/assignment-submission/eURIS/activity-algorithm-structures/attempt
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 18)
+        {
+            Console.WriteLine("You are able to vote");
+        }
+        else
+        {
+            Console.WriteLine("You are not abel to vote");
+        }
+
+        return;
+    }
+}
