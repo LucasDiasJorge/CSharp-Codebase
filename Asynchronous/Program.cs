0097046d782dfdd7f8f3a20e8a3d2de41805de8d
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-04-03 10:15:48 -0300

    Creating Asynchronous

---
+++ Asynchronous/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Asynchronous;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine(await GetDataAsync());
+    }
+
+    public static string GetDataFromApi()
+    {
+        return "Hello, World!";
+    }
+    
+    public static async Task<string> GetDataAsync()
+    {
+        return await Task.Run(() => $"Data from Asynchronous API: {GetDataFromApi()}");
+    }
+}
