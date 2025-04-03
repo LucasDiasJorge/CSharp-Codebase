928f7781cac6dc68376eaa309497e8c832f088c6
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-04-03 10:30:51 -0300

    Update Program.cs

--- Asynchronous/Program.cs
+++ Asynchronous/Program.cs
@@ -8,6 +8,7 @@ class Program
     static async Task Main(string[] args)
     {
         Console.WriteLine(await GetDataAsync());
+        await Task.Run(() => PerformRequestWithHandling());
     }
 
     public static string GetDataFromApi()
@@ -21,4 +22,20 @@ class Program
         await Task.Delay(5000);
         return await Task.Run(() => $"{GetDataFromApi()}");
     }
+
+    public static async Task PerformRequestWithHandling()
+    {
+        try
+        {
+            HttpClient client = new HttpClient();
+            string data = await client.GetStringAsync("https://example.com/data");
+            Console.WriteLine(data);
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
+        }
+    }
+    
 }
