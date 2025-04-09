8a55c470b9b2979482c3bb855a62ba09383923da
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-04-09 10:53:53 -0300

    Creating Linq example

---
+++ Linq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Data Source
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        // LINQ Query
+        var evenNumbers = numbers.Where(num => num % 2 == 0);
+        
+        // Execution
+        foreach (var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Linq/README.md
@@ -0,0 +1,3 @@
+- Artigo de referência:
+
+https://medium.com/@ravipatel.it/introduction-to-linq-in-c-26bf70607d14
