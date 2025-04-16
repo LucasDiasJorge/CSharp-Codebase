63df685d59364b8c9f8c2f4e2c9a8c3908ad949b
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-04-16 10:43:19 -0300

    Create ProductModel.cs

---
+++ SimpleWebAPI/Models/ProductModel.cs
@@ -0,0 +1,17 @@
+namespace SimpleWebAPI.Models;
+
+public class ProductModel
+{
+    public long Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public ProductModel(long id, string name, string description, decimal price)
+    {
+        Id = id;
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+}
