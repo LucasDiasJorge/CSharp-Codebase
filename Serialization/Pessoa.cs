f6c120336b782351c307d04140755b820c74bea8
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-04-27 23:40:38 -0300

    Creation of Serialization project
    https://www.coursera.org/learn/back-end-development-with-dotnet/supplement/EeDhn/introduction-to-serialization

---
+++ Serialization/Pessoa.cs
@@ -0,0 +1,7 @@
+﻿namespace Serialization;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var pessoaSerialize = new Pessoa { Nome = "Lucas", Idade = 22 };
+        string jsonStringSerialize = JsonSerializer.Serialize(pessoaSerialize);
+        
+        Console.WriteLine(jsonStringSerialize);
+        // Saída: {"Nome":"Lucas","Idade":22}
+        
+        string jsonStringDeserialize = "{\"Nome\":\"Maria\",\"Idade\":25}";
+
+        Pessoa pessoaDeserialize = JsonSerializer.Deserialize<Pessoa>(jsonStringDeserialize);
+
+        Console.WriteLine(pessoaDeserialize.Nome);  // Maria
+        Console.WriteLine(pessoaDeserialize.Idade); // 25
+
+    }
+}
