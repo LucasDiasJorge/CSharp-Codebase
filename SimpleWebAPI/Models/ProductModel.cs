ab0b6abebb0295d679c5d4126b32e84490f229da
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2025-06-12 23:06:18 -0300

    Implementing a ValidationAttribute that verify price rules

--- SimpleWebAPI/Models/ProductModel.cs
+++ SimpleWebAPI/Models/ProductModel.cs
@@ -3,8 +3,12 @@ namespace SimpleWebAPI.Models;
 public class ProductModel
 {
     public long Id { get; set; }
+    
     public string Name { get; set; }
+    
     public string Description { get; set; }
+
+    [Price]
     public decimal Price { get; set; }
 
     public ProductModel(long id, string name, string description, decimal price)
