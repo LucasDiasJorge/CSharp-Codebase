77b88999ae8b739dbbfc6094b24ba94e936df6a4
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2026-01-01 21:28:56 -0300

    PersistencePatterns: add core interfaces (IEntity, IRepository, IUnitOfWork)

---
+++ PersistencePatterns/Core/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+namespace PersistencePatterns.Core;
+
+/// <summary>
+/// Interface Unit of Work para gerenciamento de transações
+/// </summary>
+public interface IUnitOfWork : IDisposable
+{
+    Task<int> SaveChangesAsync(CancellationToken ct = default);
+    Task BeginTransactionAsync(CancellationToken ct = default);
+    Task CommitAsync(CancellationToken ct = default);
+    Task RollbackAsync(CancellationToken ct = default);
+}
