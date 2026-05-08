dfc1ecadae15c1ad41d4752454e9b299de85929a
Lucas Dias Jorge <lucas_jorg@hotmail.com>
2026-05-08 00:08:38 -0300

    chore(caching/redismetadata): add Program example

---
+++ 06-Caching/Caching/RedisMetaData/Program.cs
@@ -0,0 +1,16 @@
+﻿using RedisMetaData.Models;
+using RedisMetaData.Services;
+
+RedisConfig config = new()
+{
+    Host = "localhost",
+    Port = 6379,
+    Database = 0
+};
+
+using RedisCache cache = new(config);
+
+cache.SetEntry("user:1", "name", "Alice", TimeSpan.FromMinutes(10));
+string? name = cache.GetEntry<string>("user:1", "name");
+
+Console.WriteLine($"User name: {name}");
